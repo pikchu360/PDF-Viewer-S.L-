b086c64adbed8fcc0cbe515230a4d8c1ca01ebe6
pikchu360 <risso.leonel.92@gmail.com>
2017-06-26 21:50:45 -0300

    Add Methods
    In the Class PDWrite

--- src/pdf/Reader/PDWrite.java
+++ src/pdf/Reader/PDWrite.java
@@ -1,14 +1,41 @@
 package pdf.Reader;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+
+
 public class PDWrite {
 
 	//Attributes
+	private File fArchivo;
+	private PDDocument pdDocumento;
 	
 	//Builds
-	public PDWrite(){
-		
+	public PDWrite(String path) throws InvalidPasswordException, IOException{
+	//	leerPDF(path);
+		System.out.println();
+	}
+	
+	//Getters and setters
+	public File getArchivo(){
+		return this.fArchivo;
+	}
+	public PDDocument getDocumento(){
+		return this.pdDocumento;
 	}
 	
 	//General Methods
+	public void leerPDF(String ruta) throws InvalidPasswordException, IOException{
+		this.fArchivo = new File(ruta);
+		this.pdDocumento = PDDocument.load(fArchivo);
+	}
 	
+	
+	//Test Class PDFWrite
+	public static void main(String [] args){
+		System.out.println("Hola");
+	}
 }
